184ef7ce2fa1ffa0e596bec63af78462ca15e3d6
Sophie-cyber-dev <sophiagithub3@gmail.com>
2026-02-26 12:22:26 -0300

    feat: adicionando imagens do bg

--- src/components/sections/QuemSomos.tsx
+++ src/components/sections/QuemSomos.tsx
@@ -1,16 +1,31 @@
-/** 
- * Aqui faz os imports. Exemplo:
- * import Image from "next/image";
- * import LogoRocket from "@/assets/logo-rocket.svg"
- */
+import Image from "next/image";
+import EquipeRocketFoto from "@/assets/Img_EquipeRocketJR_Section_QuemSomos.png"
+import FogueteAnimado from "@/assets/Foguete_Animado_Section_QuemSomos.svg"
+
 
 function QuemSomos() {
   return (
-    <section className="bg-white dark:bg-black min-h-100 px-4 sm:px-6">
-      <div className="max-w-7xl mx-auto">
-        <h2 className="text-center text-white text-3xl font-bold mb-12">Quem Somos</h2>
-        {/* Adicione os logos/cards dos clientes aqui */}
-      </div>
+    <section className="min-h-screen flex flex-col justify-center items-center bg-[#000000] gap-5">
+      <h1 className="text-3xl text-color-[#FFFFFF] font-bold text-center mb-4">Quem Somos</h1>
+
+       <Image className="z-0 mt-6"
+        src={FogueteAnimado}
+        alt="Foguete vermelho com animação da RocketJR"
+      />
+
+      <Image className="mt-6 w-2xl h-[27.6 rem] "
+        src={EquipeRocketFoto}
+        alt="Foto da equipe RocketJR"
+      />
+
+
+
+
+      <p className="mt-8 text-center text-xl text-red-600">ATENÇÃO TEXTO DE EXEMPLO (AINDA NÃO TEM O TEXTO OFICIAL DO SITE) <br /></p>
+      <p className="text-center text-xl text-color-[#E7E7E7]">
+        Somos uma empresa apaixonada por tecnologia e inovação.<br></br>Nossa missão é transformar ideias em soluções digitais que impulsionam negócios.
+      </p>
+
     </section>
   );
 }
