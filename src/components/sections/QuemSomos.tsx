b4a4988b6477af543b760347b5f1129191d632bd
Sophie-cyber-dev <sophiagithub3@gmail.com>
2026-02-26 22:53:58 -0300

    feat:adicionando as imagens de bg

--- src/components/sections/QuemSomos.tsx
+++ src/components/sections/QuemSomos.tsx
@@ -9,7 +9,9 @@ function QuemSomos() {
   return (
     
 
-      <section className="min-h-screen flex flex-col justify-center items-center bg-[#000000] gap-5">
+      <section className="min-h-screen flex flex-col justify-center items-center bg-[#000000] gap-5   bg-[url('/Foguete_Animado_Section_QuemSomos.svg'),url('/Logo_Rocket_Animada_Section_QuemSomos.svg')] 
+      bg-position-[left_top_210px,right_center] 
+      bg-no-repeat bg-contain z-0 animate-background-flow">
         
         
 
@@ -25,8 +27,8 @@ function QuemSomos() {
         />
 
 
-        <p className="text-center lg:text-base text-color-[#E7E7E7]">
-        A Rocket Júnior (RJR) É A Empresa Júnior do Curso De Sistemas De Informação Do Campos Arapiraca - Unidade Educacional De Penedo.Somos Um Time De Estudantes Apaixonados Por Tecnologia E Inovação,Que Aplicam Na Prática Os Conhecimentos Adquiridos Em Sala De Aula Para Desenvolver Soluções Digitais Para O Mercado.Nosso Principal Objetivo É Proporcionar Aos Nossos Membros Uma Experiência Empresarial Real, Preparando-Os Para Os Desafios Do Mercado De Trabalho E Impulsionando Suas Carreiras!
+        <p className="text-center text-[14px] lg:text-base text-color-[#E7E7E7]">
+        A Rocket Júnior (RJR) É A Empresa Júnior do Curso De Sistemas De Informação Do Campos Arapiraca - Unidade Educacional De Penedo.Somos Um Time De Estudantes Apaixonados Por Tecnologia E Inovação,Que Aplicam Na Prática Os Conhecimentos Adquiridos Em Sala De Aula Para Desenvolver Soluções Digitais Para O Mercado. Nosso Principal Objetivo É Proporcionar Aos Nossos Membros Uma Experiência Empresarial Real, Preparando-Os Para Os Desafios Do Mercado De Trabalho E Impulsionando Suas Carreiras!
         </p>
 
         
