e983898e04b54a093499090d93a112ca82bc417b
GabrielFideles <gabrielfidelesjgfs@gmail.com>
2026-03-05 14:39:50 -0300

    fix: corrigir arquivos de conflitos da dev

--- src/components/sections/Clientes.tsx
+++ src/components/sections/Clientes.tsx
@@ -6,7 +6,7 @@
 
 export default function Clientes() {
   return (
-    <section className="bg-black min-h-[400px] py-20 px-6">
+    <section className="bg-white dark:bg-black min-h-100 px-4 sm:px-6">
       <div className="max-w-7xl mx-auto">
         <h2 className="text-center text-white text-3xl font-bold mb-12">Nossos Clientes</h2>
         {/* Adicione os logos/cards dos clientes aqui */}
