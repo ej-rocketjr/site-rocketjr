15e268b75d33e1934d8412dcd15e270bee3f7d6a
GabrielFideles <gabrielfidelesjgfs@gmail.com>
2026-03-05 14:08:43 -0300

    refactor: ajustar estrutura do hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 <<<<<<< HEAD
 import Image from "next/image";
-import SectionExemplo from "@/components/sections/SectionExemplo"
+import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
+<<<<<<< HEAD
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         
@@ -29,5 +30,10 @@ export default function Home() {
         <Footer />
     </main>
 >>>>>>> ac99ed7 (feat: adicionar header)
+=======
+    <main>
+      <Hero />
+    </main> 
+>>>>>>> 4b09484 (refactor: ajustar estrutura do hero)
   );
 }
