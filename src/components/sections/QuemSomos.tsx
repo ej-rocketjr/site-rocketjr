93e6813935d0379ad0efe487d1a7cc2b9f63160b
GabrielFideles <gabrielfidelesjgfs@gmail.com>
2026-03-05 14:39:50 -0300

    fix: corrigir arquivos de conflitos da dev

--- src/components/sections/QuemSomos.tsx
+++ src/components/sections/QuemSomos.tsx
@@ -1,25 +1,17 @@
-import Image from "next/image";
-import EquipeRocketFoto from "@/assets/Img_EquipeRocketJR_Section_QuemSomos.png"
-
+/** 
+ * Aqui faz os imports. Exemplo:
+ * import Image from "next/image";
+ * import LogoRocket from "@/assets/logo-rocket.svg"
+ */
 
 function QuemSomos() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center mb-4">Quem Somos</h1>
-
-
-      <Image className="w-2xl h-[27.6 rem]"
-        src={EquipeRocketFoto}
-        alt="Foto da equipe RocketJR"
-      />
-
-
-      <p className="mt-8 text-center text-red-600">ATENÇÃO TEXTO DE EXEMPLO (AINDA NÃO TEM O TEXTO OFICIAL DO SITE) <br /></p>
-      <p className="text-center text-zinc-700 dark:text-zinc-300">
-        Somos uma empresa apaixonada por tecnologia e inovação. Nossa missão é transformar ideias em soluções digitais que impulsionam negócios.
-      </p>
-      
-    </div>
+    <section className="bg-white dark:bg-black min-h-100 px-4 sm:px-6">
+      <div className="max-w-7xl mx-auto">
+        <h2 className="text-center text-white text-3xl font-bold mb-12">Quem Somos</h2>
+        {/* Adicione os logos/cards dos clientes aqui */}
+      </div>
+    </section>
   );
 }
 export default QuemSomos;
