ea3347839ac214a5a3f8b134ebe4f15bfa02c044
Bruno Guilherme <brunogui29@outlook.com>
2026-03-05 16:40:05 -0300

    fix: correções passadas por análise

--- src/components/sections/Servicos.tsx
+++ src/components/sections/Servicos.tsx
@@ -64,7 +64,7 @@ const cards = [
 
 function Servicos() {
   return (
-    <section className="bg-white dark:bg-black min-h-100 px-4 sm:px-6">
+    <section className="bg-white dark:bg-black min-h-100 px-4 sm:px-6" id="servicos">
       <div className="max-w-7xl mx-auto">
         <h2 className="mx-auto uppercase text-center text-black dark:text-white text-xl sm:text-2xl lg:text-3xl font-medium mb-8 sm:mb-10 lg:mb-12 max-w-190">
           Transformamos suas ideias em soluções digitais inovadoras
