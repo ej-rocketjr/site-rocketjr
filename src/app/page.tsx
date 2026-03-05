73d805bce987dbb493a3e356c4e4929279f100c9
GabrielFideles <gabrielfidelesjgfs@gmail.com>
2026-03-05 14:08:43 -0300

    refactor: ajustar estrutura do hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
-import SectionExemplo from "@/components/sections/SectionExemplo"
+import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        
-        <SectionExemplo />
-
-      </main>
-    </div>
+    <main>
+      <Hero />
+    </main> 
   );
 }
