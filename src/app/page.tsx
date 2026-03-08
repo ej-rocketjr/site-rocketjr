19f33784cbee5685d5ed0e0ee20eb79e3a68061b
Bruno Guilherme <brunogui29@outlook.com>
2026-03-07 23:01:31 -0300

    fix: resolvendo erros de sintaxe e visualização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-<<<<<<< HEAD
 import Image from "next/image";
 import Hero from "@/components/sections/Hero";
 import Servicos from "@/components/sections/Servicos";
+import Clientes from "@/components/sections/Clientes";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Servicos />
-    </main> 
+      <Clientes />
+    </main>
   );
 }
