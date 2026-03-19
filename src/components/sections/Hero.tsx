771707f769ff8bec2609cd9502a16533b546b6e9
Bruno Guilherme <brunogui29@outlook.com>
2026-03-19 16:50:11 -0300

    fix:correção subtitulo e fonte

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
-import { Google_Sans, Darker_Grotesque, Archivo_Black } from "next/font/google";
+import { Darker_Grotesque, Archivo_Black } from "next/font/google";
 
-const googleSans = Google_Sans({ subsets: ["latin"] });
 const darkerGrotesque = Darker_Grotesque({ subsets: ["latin"] });
 const amsiproBold = Archivo_Black({ subsets: ["latin"], weight: "400" });
 
@@ -9,12 +8,10 @@ export default function Hero() {
   return (
     <section className={`bg-white dark:bg-black min-h-[500px] flex flex-col items-center justify-center py-10 sm:py-20 ${darkerGrotesque.className}`}>
       <div className="text-center py-20">
-        <h1 className={`text-4xl leading-tight space-y-4 md:text-6xl font-medium text-black dark:text-white mb-6 ${googleSans.className} px-4 md:px-6`}>
+        <h1 className={`text-4xl leading-tight space-y-4 md:text-6xl font-medium text-black dark:text-white mb-12 px-4 md:px-6`}>
           Chegou a hora da inovação. Venha decolar conosco.
         </h1>
-        <p className={`text-lg sm:text-4xl md:text-2xl font-normal text-black dark:text-white mt-8 ${amsiproBold.className}`}>
-          Transformamos suas ideias em soluções digitais
-        </p>
+
 
       <div className={`flex items-center justify-center ${darkerGrotesque.className}`}>
         <Link
