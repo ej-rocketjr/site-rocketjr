77959774a6779d1491827819bcf723c8548fd7d0
Bruno Guilherme <brunogui29@outlook.com>
2026-03-19 17:04:09 -0300

    fix:eliminando fontes não usadas e correção ortogŕafica

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,18 +1,14 @@
-import Image from "next/image";
-import LogoRocketHeroDark from "@/assets/logos/logo-rocket-hero-dark.svg";
-import LogoRocketHeroLight from "@/assets/logos/Logo-rocket-hero-light.svg";
 import Link from "next/link";
-import { Darker_Grotesque, Archivo_Black } from "next/font/google";
+import { Darker_Grotesque } from "next/font/google";
 
-const darkerGrotesque = Darker_Grotesque({ subsets: ["latin"] });
-const amsiproBold = Archivo_Black({ subsets: ["latin"], weight: "400" });
+const darkerGrotesque = Darker_Grotesque({ subsets: ["latin"], weight: "800" });
 
 export default function Hero() {
   return (
-    <section className={`bg-white dark:bg-black min-h-[500px] flex flex-col items-center justify-center py-10 sm:py-20 ${darkerGrotesque.className}`}>
+    <section className={`bg-white dark:bg-black min-h-[500px] flex flex-col items-center justify-center py-10 sm:py-20`}>
       <div className="text-center py-20">
-        <h1 className={`text-4xl leading-tight space-y-4 md:text-6xl font-medium text-black dark:text-white mb-12 px-4 md:px-6`}>
-          Chegou a hora da inovação. Venha decolar conosco.
+        <h1 className={`text-4xl leading-tight space-y-4 md:text-6xl font-medium text-black dark:text-white mb-12 px-4 md:px-6 `}>
+          Chegou a hora da inovação, venha decolar conosco.
         </h1>
 
 
@@ -22,16 +18,16 @@ export default function Hero() {
         >
             <button
             type="button"
-            className="bg-gradient-to-r from-red-900 to-red-700 hover:from-red-700 hover:to-gray-900 text-white font-bold py-3 px-6 w-40 rounded-md
-            transition duration-300 ease-in-out transform hover:scale-105">Contato</button>
+            className={`bg-gradient-to-r from-red-900 to-red-700 hover:from-red-700 hover:to-gray-900 text-white font-bold py-3 px-6 w-40 rounded-md
+            transition duration-300 ease-in-out transform hover:scale-105 text-xl ${darkerGrotesque.className}`}>Contato</button>
         </Link>
         <Link
           href="/#clientes"
         >
             <button
             type="button"
-            className="border-1 border-red-600 text-black dark:text-white font-bold py-3 px-6 w-40 rounded-md
-            transition duration-300 ease-in-out transform hover:scale-105">Nossos Cases</button>
+            className={`border-1 border-red-600 text-black dark:text-white font-bold py-3 px-6 w-40 rounded-md
+            transition duration-300 ease-in-out transform hover:scale-105 text-xl ${darkerGrotesque.className}`}>Nossos Cases</button>
         </Link>
       </div>
       </div>
