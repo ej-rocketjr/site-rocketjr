afaa05428fbca98908db3a51a22e23f34bd54cf8
JottaP2 <joao.marinho@arapiraca.ufal.br>
2026-03-28 08:49:40 -0300

    fix: correção de erro do lint

--- site-rocketjr/src/app/layout.tsx
+++ site-rocketjr/src/app/layout.tsx
@@ -77,7 +77,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
       >
