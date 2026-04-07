776aecc9ae50d0493e33b69b561843b2dcd56383
JottaP2 <joao.marinho@arapiraca.ufal.br>
2026-04-07 15:45:19 -0300

    feat: atualizando link de contato rocket

--- site-rocketjr/src/app/transformacao-digital/page.tsx
+++ site-rocketjr/src/app/transformacao-digital/page.tsx
@@ -41,7 +41,7 @@ export default function TransformacaoDigital() {
             </div>
         </section>
         <div className="flex justify-center lg:pl-20 mt-8 lg:mt-10 w-full relative z-30">
-            <a href="https://wa.me/558299767685?text=Ol%C3%A1!%20Vim%20pelo%20site%20da%20Rocket%20Jr.%20e%20gostaria%20de%20saber%20mais%20sobre%20como%20iniciar%20a%20transforma%C3%A7%C3%A3o%20digital%20da%20minha%20empresa." rel="noopener noreferrer">
+            <a href="https://api.whatsapp.com/send?phone=558296940657&text=Ol%C3%A1!%20Vim%20pelo%20Site%20da%20Rocket%20Jr.%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es" rel="noopener noreferrer">
             <button type="button" className="bg-gradient-to-r from-[#530101] via-[#750404] to-[#fd0404] text-white px-6 py-3 lg:px-10 lg:py-4 rounded-md font-bold uppercase tracking-widest text-xs lg:text-sm transition-all duration-300 hover:scale-105 hover:shadow-xl active:scale-95">Iniciar transformação digital</button>
             </a>
         </div>
