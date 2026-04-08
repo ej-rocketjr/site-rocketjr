bf8ed258bd2892828923b9bd17275e58936c48e5
JottaP2 <joao.marinho@arapiraca.ufal.br>
2026-04-08 16:15:34 -0300

    fix: correções de contatos dos cards e alteração na cor da font nos themas black e white

--- site-rocketjr/src/app/solucoes-digitais/page.tsx
+++ site-rocketjr/src/app/solucoes-digitais/page.tsx
@@ -41,7 +41,7 @@ export default function SolucoesDigitais() {
             </div>
         </section>
         <div className="flex justify-center lg:pl-20 mt-8 lg:mt-10 w-full relative z-30">
-              <a href="https://api.whatsapp.com/send?phone=558296940657&text=Ol%C3%A1!%20Vim%20pelo%20Site%20da%20Rocket%20Jr.%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es" rel="noopener noreferrer">
+              <a href="https://wa.me/5582996940657?text=Ol%C3%A1!%20Vim%20pelo%20site%20da%20Rocket%20Jr.%20e%20gostaria%20de%20entender%20como%20voc%C3%AAs%20podem%20me%20ajudar%20a%20gerar%20resultados%20para%20o%20meu%20neg%C3%B3cio" rel="noopener noreferrer">
                 <button type="button" className="bg-gradient-to-r from-[#530101] via-[#750404] to-[#fd0404] text-white px-6 py-3 lg:px-10 lg:py-4 rounded-md font-bold uppercase tracking-widest text-xs lg:text-sm transition-all duration-300 hover:scale-105 hover:shadow-xl active:scale-95">Quero gerar resultados agora</button>
               </a>
         </div>
